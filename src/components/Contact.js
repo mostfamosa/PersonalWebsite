7e05c49b4cfde632e98dc346d94b555fd720289b
Mostafa Mossa <mostfa.g.m@gmail.com>
2023-06-01 22:59:57 +0300

    updated server URL

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ function Contact() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact", {
+        let response = await fetch("https://victorious-rapid-iron.glitch.me/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
